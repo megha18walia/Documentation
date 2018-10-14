d5d8a6b7b1ef9227268dbff6d29dd389b1f2a5da
megha18walia <megha18walia@gmail.com>
2018-10-14 15:56:44 +0530

    Update CreateDatabase_Restaurant.sql

--- Documentation/SQLScripts Deploy/SQL Scripts/CreateDatabase_Restaurant.sql
+++ Documentation/SQLScripts Deploy/SQL Scripts/CreateDatabase_Restaurant.sql
@@ -1,14 +1,12 @@
-USE [master]
-GO
+  USE [master]
+  GO
+  IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = N'Restaurant')
+  CREATE DATABASE [Restaurant]
+  GO
 
-/****** Object:  Database [Restaurant]    Script Date: 10/7/2018 12:29:38 PM ******/
-CREATE DATABASE [Restaurant]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Restaurant', FILENAME = N'C:\Users\mwalia\Restaurant.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'Restaurant_log', FILENAME = N'C:\Users\mwalia\Restaurant_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
-GO
+
+
+
 
 
 
