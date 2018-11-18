4f115c722ab7b5c4b2e4267ccbd28bb2b6556540
Megha Walia <mwalia2@sapient.com>
2018-11-18 12:37:58 +0000

    code checked in

---
+++ Documentation/Azure/Virtual Machine/CreateManagedDisk.ps1
@@ -0,0 +1,16 @@
+﻿Login-AzAccount 
+
+$diskConfig = New-AzureRmDiskConfig -Location "East Us" -CreateOption Empty -DiskSizeGB 128 
+$dataDisk = New-AzureRmDisk -DiskName "myDataDisk" -ResourceGroupName "TestAzureResourceGroupNew" -Disk $diskConfig
+
+$vm = Get-AzureRmVM -ResourceGroupName "TestAzureResourceGroupNew" -Name "VisualStudio"
+
+$vm = Add-AzureRmVMDataDisk -VM $vm -Name "myDataDisk" -CreateOption Attach -ManagedDiskId $dataDisk.Id -Lun 1
+Update-AzureRmVM -ResourceGroupName "TestAzureResourceGroupNew" -VM $vm
+
+#Disk Added Now run the following command to mount
+Get-Disk | where PartitionStyle -EQ "RAW" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | `
+Format-Volume -FileSystem NTFS  -NewFileSystemLabel "myDataDisk" -Confirm:$false
+
+
+
---
+++ Documentation/Azure/Virtual Machine/InstallIISCustomExtensionScript.ps1
@@ -0,0 +1,16 @@
+﻿$creds = Get-Credential
+
+New-AzureRmVM -ResourceGroupName testvhd -Location "East US" -Name "TestCustomeVM" -VirtualNetworkName "myVnet" -SubnetName "mySubNet" `
+-PublicIpAddressName "pip" -Credential $creds -OpenPorts 80 -SecurityGroupName "NetworkSecurityGroup"
+
+# Custom script 
+
+
+Set-AzureRmVMExtension -ResourceGroupName "testvhd" `
+    -ExtensionName "IIS" `
+    -VMName "TestCustomeVM" `
+    -Location "EastUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
